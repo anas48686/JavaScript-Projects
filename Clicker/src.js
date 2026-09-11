263512ae56d3b0bb0b870a7e228fc74dbb080588
Mohammad Anas Hannan <mohammadanashannan@gmail.com>
2026-09-11 20:00:39 +0530

    Clicker added

---
+++ Clicker/src.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body');
+
+body.addEventListener('click', (e)=>{
+  const circleEl = document.createElement('div');
+  circleEl.classList.add('circle');
+  circleEl.textContent = "Hello";
+
+  const color = ['red', 'blue', 'pink', 'orange', 'green', 'white'];
+  circleEl.style.backgroundColor = color[Math.floor(Math.random()* color.length)];
+
+  circleEl.style.top = `${e.clientY}px`;
+  circleEl.style.left = `${e.clientX}px`;
+  
+  body.append(circleEl);
+
+  setTimeout(()=>{
+    circleEl.remove();
+  }, 5000);
+})
+
